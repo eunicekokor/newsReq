b30699e950c8e8045d9b3b0f07c2aa1ec66896fa
Sandra N Shaefer <sandra@sherlock.is.private>
2016-11-21 15:06:43 -0500

    Currently figuring out how to prevent adding duplicates to db

--- getNews.py
+++ getNews.py
@@ -1,8 +1,32 @@
 import requests
 import json
 import time
+from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch, RequestsHttpConnection
 
 apiKey = "8f37248639844a2294dbe1af7c6f3b24"
+
+YOUR_ACCESS_KEY = "AKIAIFXS6NMDG6DNTGNQ"
+YOUR_SECRET_KEY = "lwVBol6OuS32W4DA+kasw5Qv8W5HfrKFCMC9g01W"
+REGION = "us-west-2"
+
+awsauth = AWS4Auth(YOUR_ACCESS_KEY, YOUR_SECRET_KEY, REGION, 'es')
+
+host = 'search-news-req-rdi65wkzrj6inpmxnjhftkl5fi.us-west-2.es.amazonaws.com'
+
+es = Elasticsearch(
+  hosts=[{
+    'host': host,
+    'port': 443,
+  }],
+  http_auth=awsauth,
+  use_ssl=True,
+  verify_certs=True,
+  connection_class=RequestsHttpConnection
+  )
+
+es.indices.create(index='news', ignore=400)
+print (es.info())
 
 sources = {
   "business": ["bloomberg", "business-insider","cnbc","financial-times","fortune"],
@@ -36,7 +60,27 @@ for category in sources.keys():
       for article in article_list:
         article["category"] = str(category)
         article["source"] = str(source)
-        print "{}\n\n".format(article)
+        #print "{}\n\n".format(article)
 
         ## add indexing to ES
+        try:
+          in_db_result = es.search(index='news',doc_type="article",body={
+            "query": {
+              "match": {
+                "title": article['title']
+              }
+            }
+          })
+          duplicate = json.dumps(in_db_result['hits']['hits'])
+          print("Duplicate", duplicate)
+          if(len(duplicate) == 0):
+            res = es.index(index="news", doc_type='article', body=article)
+            print("Adding to db")
+            print "{}\n\n".format(article)
+          else:
+            print("Skipped adding to db")
+        except:
+          print("Error adding to db")
+          pass
+
 
--- newsReq.py
+++ newsReq.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 
 YOUR_ACCESS_KEY = "AKIAIFXS6NMDG6DNTGNQ"
 YOUR_SECRET_KEY = "lwVBol6OuS32W4DA+kasw5Qv8W5HfrKFCMC9g01W"
-REGION = "us-east-1"
+REGION = "us-west-2"
 
 awsauth = AWS4Auth(YOUR_ACCESS_KEY, YOUR_SECRET_KEY, REGION, 'es')
 
-host = ''
+host = 'search-news-req-rdi65wkzrj6inpmxnjhftkl5fi.us-west-2.es.amazonaws.com'
 
 es = Elasticsearch(
   hosts=[{
@@ -37,7 +37,13 @@ es = Elasticsearch(
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+	result = es.search(index='news',doc_type="article",body={
+		"query": { "match_all": {} },
+		"sort": { "publishedAt": { "order": "desc" } 
+		}
+	})
+	data = json.dumps(result['hits']['hits'])
+	return data
 
 
 if __name__ == '__main__':
