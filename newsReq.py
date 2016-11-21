bbe96c4b6453fb99f279390093347058dede480e
Sandra N Shaefer <sandra@sherlock.is.private>
2016-11-21 13:45:33 -0500

    Adding some AWS stuff and requirements.txt

--- newsReq.py
+++ newsReq.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+import json
+import pprint
+import sys
+from datetime import *
+import time
+import certifi
+from requests_aws4auth import AWS4Auth
 
 app = Flask(__name__)
 
 
+YOUR_ACCESS_KEY = "AKIAIFXS6NMDG6DNTGNQ"
+YOUR_SECRET_KEY = "lwVBol6OuS32W4DA+kasw5Qv8W5HfrKFCMC9g01W"
+REGION = "us-east-1"
+
+awsauth = AWS4Auth(YOUR_ACCESS_KEY, YOUR_SECRET_KEY, REGION, 'es')
+
+host = ''
+
+es = Elasticsearch(
+  hosts=[{
+    'host': host,
+    'port': 443,
+  }],
+  http_auth=awsauth,
+  use_ssl=True,
+  verify_certs=True,
+  connection_class=RequestsHttpConnection
+  )
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!1'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(port=8000)
