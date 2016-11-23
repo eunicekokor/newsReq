2076f4729dd40c07a39c8aa29d6a4794bdba85b3
Eunice Kokor <eek2138@columbia.edu>
2016-11-23 09:53:01 -0800

    removed all sensitive info

--- getNews.py
+++ getNews.py
@@ -4,14 +4,14 @@ import time
 from requests_aws4auth import AWS4Auth
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import sys
+import certifi
+from config import *
 
-apiKey = "8f37248639844a2294dbe1af7c6f3b24"
-
-YOUR_ACCESS_KEY = "AKIAIFXS6NMDG6DNTGNQ"
-YOUR_SECRET_KEY = "lwVBol6OuS32W4DA+kasw5Qv8W5HfrKFCMC9g01W"
 REGION = "us-west-2"
 
 awsauth = AWS4Auth(YOUR_ACCESS_KEY, YOUR_SECRET_KEY, REGION, 'es')
+print ("The environment is working, your access key is: {}".format(YOUR_ACCESS_KEY))
+print ("The environment is working, your secret key is: {}".format(YOUR_SECRET_KEY))
 
 host = 'search-news-req-rdi65wkzrj6inpmxnjhftkl5fi.us-west-2.es.amazonaws.com'
 
@@ -29,7 +29,7 @@ es = Elasticsearch(
 # es.indices.delete(index='news', ignore=[400, 404])
 # time.sleep(10)
 
-es.indices.create(index='news', ignore=400)
+# es.indices.create(index='news', ignore=400)
 print (es.info())
 
 sources = {
@@ -42,47 +42,48 @@ sources = {
   "science-and-nature": ["national-geographic", "new-scientist"],
   "technology": ["techcrunch","wired-de","ars-technica", "engadget", "hacker-news", "recode"]
 }
-
 # When we're building up our news db, our request will query each category
 # and request articles from each source. Sort by latest, popular, and top
 # Example API Request -->
-count = 0
-for category in sources.keys():
-  source_list = sources[category]
-  for source in source_list:
-    request_url = "https://newsapi.org/v1/articles?source={source}&apiKey={apiKey}".format(source=source, apiKey=apiKey)
-    try:
-      resp = requests.get(request_url)
-    except:
-      time.sleep(10)
-      resp = requests.get(request_url)
+def fetchArticles(sources):
+  count = 0
+  for category in sources.keys():
+    source_list = sources[category]
+    for source in source_list:
+      request_url = "https://newsapi.org/v1/articles?source={source}&apiKey={apiKey}".format(source=source, apiKey=APIKEY)
+      try:
+        resp = requests.get(request_url)
+      except:
+        time.sleep(10)
+        resp = requests.get(request_url)
 
-    json_data = resp.json()
-    if json_data.get('articles'):
-      article_list = json_data['articles']
-      print("Number of Articles", len(article_list), "total", count)
-      count += len(article_list)
-      print "Articles for {} in the category of {}".format(source, category)
-      for article in article_list:
-        for v in article.keys():
-          if article[v]:
-            article[v] = article[v].encode('ascii', 'ignore')
-        article["category"] = str(category)
-        article["source"] = str(source)
+      json_data = resp.json()
+      if json_data.get('articles'):
+        article_list = json_data['articles']
+        no_articles = len(article_list)
+        print("Number of Articles", no_articles, "total", count)
+        count += no_articles
+        print ("Articles for {} in the category of {}".format(source, category))
+        for article in article_list:
+          for v in article.keys():
+            if article[v]:
+              article[v] = article[v].encode('ascii', 'ignore')
+          article["category"] = str(category)
+          article["source"] = str(source)
 
-        ## add indexing to ES
-        try:
-          dup = es.get(index="news", doc_type='article', id=article["title"])
-          print("Article "
-        except:
-          print("Article not in db, adding article", sys.exc_info()[0])
+          ## add indexing to ES
           try:
-            res = es.index(index="news", doc_type='article', id=article["title"], body=article)
-            print "{}\n\n".format(article)
+            dup = es.get(index="news", doc_type='article', id=article["title"])
+            print("Article ")
           except:
-            print("Error adding to db", sys.exc_info()[0])
-            pass
+            print("Article not in db, adding article", sys.exc_info()[0])
+            try:
+              res = es.index(index="news", doc_type='article', id=article["title"], body=article)
+              print ("{}\n\n".format(article))
+            except:
+              print("Error adding to db", sys.exc_info()[1])
+              pass
 
+  print(count)
 
-print(count)
-
+fetchArticles(sources)
--- newsReq.py
+++ newsReq.py
@@ -7,12 +7,11 @@ from datetime import *
 import time
 import certifi
 from requests_aws4auth import AWS4Auth
+from config import *
+
 
 app = Flask(__name__)
 
-
-YOUR_ACCESS_KEY = "AKIAIFXS6NMDG6DNTGNQ"
-YOUR_SECRET_KEY = "lwVBol6OuS32W4DA+kasw5Qv8W5HfrKFCMC9g01W"
 REGION = "us-west-2"
 
 awsauth = AWS4Auth(YOUR_ACCESS_KEY, YOUR_SECRET_KEY, REGION, 'es')
@@ -39,7 +38,7 @@ es = Elasticsearch(
 def hello_world():
 	result = es.search(index='news',doc_type="article",body={
 		"query": { "match_all": {} },
-		"sort": { "publishedAt": { "order": "desc" } 
+		"sort": { "publishedAt": { "order": "desc" }
 		}
 	})
 	data = json.dumps(result['hits']['hits'])
