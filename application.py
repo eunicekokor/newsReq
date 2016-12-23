0c7c91e6bb236fdfdf26240536aad0c8c98afe7d
Ubuntu <ubuntu@ip-10-0-0-171.us-west-2.compute.internal>
2016-12-23 08:43:06 +0000

    ec2ready

--- application.py
+++ application.py
@@ -1,18 +1,17 @@
 import json
-import requests
-import tweepy
 import time
 from flask import Flask, render_template, request
 import certifi
 from requests_aws4auth import AWS4Auth
 import sys
+print sys.path
 from config import *
 import sns_receiver as sns
 
-application = Flask(__name__)
+app = Flask(__name__)
 
 # NOTE I moved this to getNews.py, so if you run getNews.py it should work for SNS
-@application.route('/notification', methods=['GET','POST'])
+@app.route('/notification', methods=['GET','POST'])
 def notification():
   print(request.method)
   if (request.method == "POST"):
@@ -26,5 +25,5 @@ def notification():
 
 
 if __name__ == "__main__":
-    application.debug = True
-    application.run(port=8000)
+    app.debug = True
+    app.run(port=8000)
